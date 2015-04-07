74ee54c33c143019cb95159dbb94385293df14ae
Jan Valentik <johny.valentik@gmail.com>
2015-04-07 21:01:00 +0200

    interface and Singleton

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DayCalc
 First attempt at Day Calculator in JAVA
+Uses Singleton design pattern and Interface
---
+++ src/org/jvk/DisplayDate.java
@@ -0,0 +1,8 @@
+package org.jvk;
+
+/**
+ * Created by powerj-sk on 07/04/2015.
+ */
+public interface DisplayDate {
+    void SayDate();
+}
